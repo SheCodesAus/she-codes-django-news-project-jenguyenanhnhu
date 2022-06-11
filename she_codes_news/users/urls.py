f99043f881f7f5f08205f20da717992e3d413fcd
jenguyenanhnhu <102585311+jenguyenanhnhu@users.noreply.github.com>
2022-06-11 19:25:30 +1000

    Create an Account in Nav test

--- she_codes_news/users/urls.py
+++ she_codes_news/users/urls.py
@@ -3,4 +3,5 @@ from .views import CreateAccountView
 
 app_name = 'users'
 urlpatterns = [
-    path('create-account/', CreateAccountView.as_view(), name='createAccount'),]
+    path('create-account/', CreateAccountView.as_view(), name='createAccount'),
+    ]
